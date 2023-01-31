37ab8731130d667bd673ec8c700aad09406201c5
Fernando Blanco <fernandoblancovaldez@gmail.com>
2023-01-30 23:37:01 -0300

    presentation of the 1st prototype with js css and structure (no cv xp or skills data)

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const url = "https://randomuser.me/apis/";
+const url = "https://randomuser.me/api/";
+const d = document,
+  $main = d.querySelector("main");
 
 async function ajax(props) {
-  const d = document;
   let { url, cbSuccess } = props;
 
-  d.querySelectorAll(".loader").style.display = "block"; /* 
-  d.getElementsByClassName("loader").style.display = "block"; */
+  /* d.getElementsByClassName("loader").style.display = "block"; */
+  /* d.querySelectorAll(".loader").style.display = "block"; */
 
   await fetch(url)
     .then((res) => (res.ok ? res.json() : Promise.reject(res)))
     .then((json) => cbSuccess(json))
     .catch((err) => {
-      console.log(err);
       let message = err.statusText || `Ocurrió un error al acceder a la API`;
-      document.getElementsByClassName("card").innerHTML = `
+      $main.innerHTML = `
       <div >
         <p>Error ${err.status}: ${message}</p>
       </div>      
@@ -27,9 +27,74 @@ await ajax({
   cbSuccess: (json) => {
     const person = json.results[0];
     console.log(person);
+
+    d.querySelector(".card").insertAdjacentHTML(
+      "afterbegin",
+      `<div><h1>${person.name.first} ${person.name.last}</h1><h3>Front end Web Developer</h3></div>`
+    );
+    d.querySelector(".profile-pic img").src = person.picture.large;
+    d.querySelector(".profile-pic img").style =
+      "border-radius : 50%; border: 0.5rem solid #ffffff";
+
+    d.querySelector(".info").insertAdjacentHTML(
+      "afterbegin",
+      ` <p>
+            <b>Profesional independiente, proactivo y autodidacta</b> que se desenvuelve en el área de desarrollo web.<br />
+            <br />
+            <em>
+            Busco continuamente incorporar nuevas tecnologías que nos permitan al equipo y a mi optimizar el trabajo del día a día.
+            </em>
+            <br />
+            <br />
+            <b><em>A continuación ofrezco en mas detalle mis experiencias y habilidades adquiridas hasta el momento</em></b>
+        </p>`
+    );
+
+    d.querySelector(".loader").style = "display: none";
+
+    d.addEventListener("click", (e) => {
+      if (e.target.matches("#envelope") || e.target.matches("#envelope *")) {
+        d.querySelector(".info").innerHTML = `
+            <p>mi email es</p>
+            <h2>${person.email}</h2>
+            `;
+      }
+      if (
+        e.target.matches(".profile-pic") ||
+        e.target.matches(".profile-pic *")
+      ) {
+        d.querySelector(".info").innerHTML = ` <p>
+            <b>Profesional independiente, proactivo y autodidacta</b> que se desenvuelve en el área de desarrollo web.<br />
+            <br />
+            <em>
+            Busco continuamente incorporar nuevas tecnologías que nos permitan al equipo y a mi optimizar el trabajo del día a día.
+            </em>
+            <br />
+            <br />
+            <b><em>A continuación ofrezco en mas detalle mis experiencias y habilidades adquiridas hasta el momento</em></b>
+        </p>`;
+      }
+      if (e.target.matches("#calendar") || e.target.matches("#calendar *")) {
+        d.querySelector(".info").innerHTML = `
+            <p>mi fecha de nacimiento es</p>
+            <h2>${person.registered.date}</h2>
+            `;
+      }
+      if (e.target.matches("#location") || e.target.matches("#location *")) {
+        d.querySelector(".info").innerHTML = `
+            <p>vivo en</p>
+            <h2>${person.location.street.name} ${person.location.street.number} - ${person.location.city}, ${person.location.country}</h2>
+            `;
+      }
+      if (e.target.matches("#phone") || e.target.matches("#phone *")) {
+        d.querySelector(".info").innerHTML = `
+            <p>mi teléfono es</p>
+            <h2>${person.phone}</h2>
+            `;
+      }
+    });
   },
 });
-
 /* fetch("https://randomuser.me/api/")
   .then((res) => res.json())
   .then((json) => console.log(json);
